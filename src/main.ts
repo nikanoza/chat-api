b60896c9e1f9050c75ac774a6c3f861a8bdba76e
Nika Nozadze <nikanozadze1010@gmail.com>
2023-09-19 18:33:59 +0400

    feat: connect to mongo database

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connect from "./config/mongo.js";
+
+dotenv.config();
+connect();
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
 app.listen(process.env.PORT || 4444);
